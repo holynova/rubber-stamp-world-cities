5046aaf47f04ca535604d18c55625d341511cd82
holy_nova <holy_nova@126.com>
2026-09-02 13:40:10 +0800

    Create world city rubber stamp poster gallery

---
+++ data.js
@@ -0,0 +1,332 @@
+window.CITIES = [
+  {
+    "id": 1,
+    "city": "上海",
+    "features": "外滩天际线、东方明珠、黄浦江",
+    "keywords": [
+      "外滩",
+      "江湾",
+      "摩登"
+    ],
+    "output": "images/01_上海.png"
+  },
+  {
+    "id": 2,
+    "city": "北京",
+    "features": "故宫屋顶、天坛祈年殿、古城轴线",
+    "keywords": [
+      "宫城",
+      "古都",
+      "中轴"
+    ],
+    "output": "images/02_北京.png"
+  },
+  {
+    "id": 3,
+    "city": "东京",
+    "features": "东京塔、密集城市天际线、富士山远景",
+    "keywords": [
+      "霓虹",
+      "秩序",
+      "远山"
+    ],
+    "output": "images/03_东京.png"
+  },
+  {
+    "id": 4,
+    "city": "京都",
+    "features": "清水寺木构、层叠屋檐、东山山影",
+    "keywords": [
+      "古寺",
+      "竹影",
+      "静谧"
+    ],
+    "output": "images/04_京都.png"
+  },
+  {
+    "id": 5,
+    "city": "首尔",
+    "features": "南山首尔塔、韩屋屋顶、群山城市轮廓",
+    "keywords": [
+      "山城",
+      "韩屋",
+      "夜色"
+    ],
+    "output": "images/05_首尔.png"
+  },
+  {
+    "id": 6,
+    "city": "曼谷",
+    "features": "大皇宫尖顶、湄南河、热带棕榈",
+    "keywords": [
+      "金顶",
+      "河流",
+      "热带"
+    ],
+    "output": "images/06_曼谷.png"
+  },
+  {
+    "id": 7,
+    "city": "新加坡",
+    "features": "滨海湾金沙、摩天轮、滨海树影",
+    "keywords": [
+      "海湾",
+      "花园",
+      "未来"
+    ],
+    "output": "images/07_新加坡.png"
+  },
+  {
+    "id": 8,
+    "city": "孟买",
+    "features": "印度门、阿拉伯海海岸线、殖民建筑轮廓",
+    "keywords": [
+      "海港",
+      "石拱",
+      "活力"
+    ],
+    "output": "images/08_孟买.png"
+  },
+  {
+    "id": 9,
+    "city": "迪拜",
+    "features": "哈利法塔、沙丘、现代天际线",
+    "keywords": [
+      "沙漠",
+      "高塔",
+      "奢华"
+    ],
+    "output": "images/09_迪拜.png"
+  },
+  {
+    "id": 10,
+    "city": "伊斯坦布尔",
+    "features": "圣索菲亚大教堂圆顶、清真寺尖塔、海峡",
+    "keywords": [
+      "圆顶",
+      "海峡",
+      "交汇"
+    ],
+    "output": "images/10_伊斯坦布尔.png"
+  },
+  {
+    "id": 11,
+    "city": "莫斯科",
+    "features": "圣瓦西里大教堂洋葱顶、红场、克里姆林宫",
+    "keywords": [
+      "红场",
+      "穹顶",
+      "寒色"
+    ],
+    "output": "images/11_莫斯科.png"
+  },
+  {
+    "id": 12,
+    "city": "巴黎",
+    "features": "埃菲尔铁塔、塞纳河、城市屋顶",
+    "keywords": [
+      "铁塔",
+      "河岸",
+      "优雅"
+    ],
+    "output": "images/12_巴黎.png"
+  },
+  {
+    "id": 13,
+    "city": "伦敦",
+    "features": "大本钟、伦敦眼、泰晤士河",
+    "keywords": [
+      "钟楼",
+      "雾气",
+      "河流"
+    ],
+    "output": "images/13_伦敦.png"
+  },
+  {
+    "id": 14,
+    "city": "罗马",
+    "features": "斗兽场、古典柱廊、松树",
+    "keywords": [
+      "遗迹",
+      "石柱",
+      "永恒"
+    ],
+    "output": "images/14_罗马.png"
+  },
+  {
+    "id": 15,
+    "city": "巴塞罗那",
+    "features": "圣家堂尖塔、地中海天际线、彩色屋顶",
+    "keywords": [
+      "高迪",
+      "海风",
+      "曲线"
+    ],
+    "output": "images/15_巴塞罗那.png"
+  },
+  {
+    "id": 16,
+    "city": "阿姆斯特丹",
+    "features": "运河屋、窄立面、桥与自行车道路",
+    "keywords": [
+      "运河",
+      "砖屋",
+      "桥梁"
+    ],
+    "output": "images/16_阿姆斯特丹.png"
+  },
+  {
+    "id": 17,
+    "city": "雅典",
+    "features": "卫城帕特农神庙、石灰岩山丘、橄榄树",
+    "keywords": [
+      "卫城",
+      "石丘",
+      "古典"
+    ],
+    "output": "images/17_雅典.png"
+  },
+  {
+    "id": 18,
+    "city": "开罗",
+    "features": "吉萨金字塔、沙漠地平线、尼罗河绿洲",
+    "keywords": [
+      "金字塔",
+      "沙漠",
+      "尼罗"
+    ],
+    "output": "images/18_开罗.png"
+  },
+  {
+    "id": 19,
+    "city": "内罗毕",
+    "features": "城市天际线、远方草原、长颈鹿剪影",
+    "keywords": [
+      "草原",
+      "高原",
+      "野性"
+    ],
+    "output": "images/19_内罗毕.png"
+  },
+  {
+    "id": 20,
+    "city": "开普敦",
+    "features": "桌山、海湾、海岸道路",
+    "keywords": [
+      "桌山",
+      "海角",
+      "海风"
+    ],
+    "output": "images/20_开普敦.png"
+  },
+  {
+    "id": 21,
+    "city": "纽约",
+    "features": "帝国大厦、布鲁克林大桥、曼哈顿天际线",
+    "keywords": [
+      "高楼",
+      "桥梁",
+      "能量"
+    ],
+    "output": "images/21_纽约.png"
+  },
+  {
+    "id": 22,
+    "city": "旧金山",
+    "features": "金门大桥、起伏街道、海湾",
+    "keywords": [
+      "金门",
+      "坡道",
+      "海湾"
+    ],
+    "output": "images/22_旧金山.png"
+  },
+  {
+    "id": 23,
+    "city": "墨西哥城",
+    "features": "宪法广场、都市天际线、远方火山",
+    "keywords": [
+      "高原",
+      "广场",
+      "火山"
+    ],
+    "output": "images/23_墨西哥城.png"
+  },
+  {
+    "id": 24,
+    "city": "里约热内卢",
+    "features": "基督像、糖面包山、海湾曲线",
+    "keywords": [
+      "山海",
+      "热带",
+      "弧线"
+    ],
+    "output": "images/24_里约热内卢.png"
+  },
+  {
+    "id": 25,
+    "city": "布宜诺斯艾利斯",
+    "features": "方尖碑、欧式街区、普拉塔河",
+    "keywords": [
+      "方尖碑",
+      "街区",
+      "探戈"
+    ],
+    "output": "images/25_布宜诺斯艾利斯.png"
+  },
+  {
+    "id": 26,
+    "city": "利马",
+    "features": "悬崖海岸、太平洋、殖民建筑",
+    "keywords": [
+      "海崖",
+      "太平洋",
+      "土色"
+    ],
+    "output": "images/26_利马.png"
+  },
+  {
+    "id": 27,
+    "city": "悉尼",
+    "features": "歌剧院、海港大桥、蓝色海湾",
+    "keywords": [
+      "歌剧院",
+      "港桥",
+      "海蓝"
+    ],
+    "output": "images/27_悉尼.png"
+  },
+  {
+    "id": 28,
+    "city": "墨尔本",
+    "features": "有轨电车、维多利亚建筑、城市河岸",
+    "keywords": [
+      "电车",
+      "街巷",
+      "文艺"
+    ],
+    "output": "images/28_墨尔本.png"
+  },
+  {
+    "id": 29,
+    "city": "温哥华",
+    "features": "群山、海湾、城市天际线、针叶林",
+    "keywords": [
+      "雪山",
+      "海湾",
+      "松林"
+    ],
+    "output": "images/29_温哥华.png"
+  },
+  {
+    "id": 30,
+    "city": "库斯科",
+    "features": "石墙、山地梯田、安第斯山脉",
+    "keywords": [
+      "石城",
+      "梯田",
+      "高原"
+    ],
+    "output": "images/30_库斯科.png"
+  }
+];
